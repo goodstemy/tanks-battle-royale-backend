f0ac7b8f17ed27089f0e2843ce272d113f697603
Ivan Efremov <ivan.efremov@agriledger.com>
2019-05-08 23:45:59 +0300

    WIP

--- server.js
+++ server.js
@@ -1,36 +1,30 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-let players = {}
+let players = [];
 
 io.on('connection', socket => {
-	console.log(`${socket.id} connected`)
-	players[socket.id] = {
-		rotation: 0,
-		x: Math.floor(Math.random() * 200) + 50,
-		y: Math.floor(Math.random() * 200) + 50,
-		playerId: socket.id
-	}
+  console.log(`${socket.id} connected`);
 
-	socket.emit('currentPlayers', players)
-	socket.broadcast.emit('newPlayer', players[socket.id])
+  socket.on('tank info', (info) => {
+    info.id = socket.id;
+    if (!players.includes(socket.id)) {
+      players.push(socket.id);
+      socket.broadcast.emit('new player', info);
+    }
+
+    io.emit('tanks info', info);
+  });
 
 	socket.on('disconnect', () => {
-		console.log(`${socket.id} user disconnected`)
-		delete players[socket.id]
-		io.emit('disconnect', socket.id)
-	})
+		console.log(`${socket.id} user disconnected`);
+		const index = players.indexOf(socket.id);
+		players = players.splice(index, index);
+	});
 
-	socket.on('playerMovement', function(movementData) {
-		players[socket.id].x = movementData.x
-		players[socket.id].y = movementData.y
-		players[socket.id].rotation = movementData.rotation
-
-		socket.broadcast.emit('playerMoved', players[socket.id])
-	})
-})
+});
 
 http.listen(3000, function() {
 	console.log('listening on *:3000')
-})
+});
