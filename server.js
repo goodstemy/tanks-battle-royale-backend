198dcf6968f9a450ab5751ff36288a3a318ddd56
rob3n <roben1006@gmail.com>
2019-05-09 13:54:54 +0300

    try broadcast disconnected socket id

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
-let players = [];
+let players = []
 
 io.on('connection', socket => {
-  console.log(`${socket.id} connected`);
+	console.log(`${socket.id} connected`)
 
-  socket.on('tank info', (info) => {
-    info.id = socket.id;
-    if (!players.includes(socket.id)) {
-      players.push(socket.id);
-      socket.broadcast.emit('new player', info);
-    }
+	socket.on('tank info', info => {
+		info.id = socket.id
+		if (!players.includes(socket.id)) {
+			players.push(socket.id)
+			socket.broadcast.emit('new player', info)
+		}
 
-    io.emit('tanks info', info);
-  });
+		io.emit('tanks info', info)
+	})
 
-	socket.on('disconnect', () => {
-		console.log(`${socket.id} user disconnected`);
-		const index = players.indexOf(socket.id);
-		players = players.splice(index, index);
-	});
-
-});
+	socket.on('disconnect', id => {
+		console.log(`${socket.id} user disconnected`)
+		const index = players.indexOf(socket.id)
+		players = players.splice(index, index)
+		socket.broadcast.emit('disconnect', socket.id)
+	})
+})
 
 http.listen(3000, function() {
 	console.log('listening on *:3000')
-});
+})
