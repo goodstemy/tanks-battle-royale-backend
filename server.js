e5f1b7f582b09fb436521066b035548820f52f3e
Ivan Efremov <ivan.efremov@agriledger.com>
2019-05-11 10:46:32 +0300

    Refactor & fix

--- server.js
+++ server.js
@@ -1,31 +1,30 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-let players = []
+let players = [];
 
 io.on('connection', socket => {
 	console.log(`${socket.id} connected`)
 
-	socket.on('tank info', info => {
+	socket.on('tank info', (info) => {
 		info.id = socket.id
 		if (!players.includes(socket.id)) {
-			players.push(socket.id)
-			socket.broadcast.emit('new player', info)
+			players.push(socket.id);
+			socket.broadcast.emit('new player', info);
 		}
 
-		io.emit('tanks info', info)
-	})
+		io.emit('tanks info', info);
+	});
 
-	socket.on('disconnect', id => {
-		console.log(`${socket.id} user disconnected`)
-		const index = players.indexOf(socket.id)
-		players = players.splice(index, index)
-		io.emit('disconnect', socket.id)
-		// socket.broadcast.emit('disconnect', socket.id)
-	})
-})
+	socket.on('disconnect', (id) => {
+		console.log(`${socket.id} user disconnected`);
+		const index = players.indexOf(socket.id);
+		players = players.splice(index, index);
+		socket.broadcast.emit('player disconnect', socket.id);
+	});
+});
 
-http.listen(3000, function() {
+http.listen(3000, () => {
 	console.log('listening on *:3000')
-})
+});
